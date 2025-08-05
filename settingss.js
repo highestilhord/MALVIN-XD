a4c3581bd2d485901a8303b5f82e0e32979005ec
highestilhord <piusantw0596@gmail.com>
2025-08-05 00:34:31 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "",  // malvin~879HBbqJ#TMA4ILyXO56qZviwsAovhop1FGMLbqhSbNE4EFwgsFk
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "263714757857", // 233208064824
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
   TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
-};
+};malvin~879HBbqJ#TMA4ILyXO56qZviwsAovhop1FGMLbqhSbNE4EFwgsFk
